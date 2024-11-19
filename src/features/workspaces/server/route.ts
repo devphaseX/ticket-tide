22f806db4b7dcfff674c5099d23200d9cdc617ef
Ayomide Lawal <ayomidelawal700@gmail.com>
2024-11-19 16:22:38 +0100

    allow user to access workspace by membership

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -3,18 +3,40 @@ import { Hono } from "hono";
 import { createWorkspaceSchema } from "../schema";
 import { sessionMiddleware } from "@/lib/session_middleware";
 import { env } from "@/lib/env";
-import { AppwriteException, ID } from "node-appwrite";
+import { AppwriteException, ID, Query } from "node-appwrite";
 import { errorResponse, successResponse } from "@/lib/api_response";
 
 import StatusCodes from "http-status";
+import { MemberRole } from "@/features/members/member.types";
 
 const app = new Hono()
   .get("/", sessionMiddleware, async (c) => {
-    const database = c.get("databases");
-    const workspaces = await database.listDocuments(
+    const user = c.get("user");
+    const db = c.get("databases");
+
+    const workspaceMemberships = await db.listDocuments(
+      env.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
+      env.NEXT_PUBLIC_APPWRITE_MEMBERS_ID,
+      [Query.equal("userId", user.$id)],
+    );
+
+    if (!workspaceMemberships.total) {
+      return successResponse(c, { documents: [], total: 0 });
+    }
+
+    const workspaces = await db.listDocuments(
       env.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
       env.NEXT_PUBLIC_APPWRITE_WORKSPACES_ID,
+      [
+        Query.orderDesc("$createdAt"),
+        Query.contains(
+          "$id",
+          workspaceMemberships.documents.map((memeber) => memeber.workspaceId),
+        ),
+      ],
     );
+
+    console.log({ workspaces });
 
     return successResponse(c, workspaces);
   })
@@ -52,6 +74,17 @@ const app = new Hono()
           env.NEXT_PUBLIC_APPWRITE_WORKSPACES_ID,
           ID.unique(),
           { name, userId: user.$id, imageUrl: uploadedImageUrl },
+        );
+
+        await db.createDocument(
+          env.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
+          env.NEXT_PUBLIC_APPWRITE_MEMBERS_ID,
+          ID.unique(),
+          {
+            userId: user.$id,
+            workspaceId: workspace.$id,
+            role: MemberRole.ADMIN,
+          },
         );
 
         return successResponse(c, { data: workspace }, StatusCodes.CREATED);
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -8,6 +8,7 @@ const envSchema = z.object({
   NEXT_PUBLIC_APPWRITE_DATABASE_ID: z.string().min(1),
   NEXT_PUBLIC_APPWRITE_WORKSPACES_ID: z.string().min(1),
   NEXT_PUBLIC_APPWRITE_IMAGES_BUCKET_ID: z.string().min(1),
+  NEXT_PUBLIC_APPWRITE_MEMBERS_ID: z.string().min(1),
 });
 
 type AppEnv = TypeOf<typeof envSchema>;
